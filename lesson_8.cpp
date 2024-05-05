380a6d46bd27369b622b144e448d4ecc9fd06ce5
ITProggerUZ <idns365@gmail.com>
2024-05-05 17:24:12 +0500

    Lesson 8

--- lesson_8.cpp
+++ lesson_8.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <istream>
 #include <string>
 
 // INFO: Insertion Operator: <<
@@ -9,11 +10,20 @@
 int main() {
   std::string name;
   // This is cout which stands for output
-  std::cout << "What's your name?\n";
-  // This is cin which stands for input
-  // NOTE: You can't do input message, like in python, but you can use cout
-  // first.
-  std::cin >> name;
+  std::cout << "What's your full name?\n";
+
+  // This is cin which stands for input (commented because of space will only
+  // get first word)
+  // NOTE: You can't do input message, like in python, but you
+  // can use cout first.
+  // NOTE: Input with spaces will be only first word is
+  // being writed
+
+  /* std::cin >> name; */
+
+  // To fix that use getline()
+  // The getline gets value from first argument and sets to the `name` variable
+  std::getline(std::cin >> std::ws, name);
   std::cout << "Hi, " << name;
 
   return 0;
