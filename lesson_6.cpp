06110a441329d93cc923413ae09712417b91cc66
ITProggerUZ <idns365@gmail.com>
2024-05-05 14:11:38 +0500

    Lesson 6 updated

--- lesson_6.cpp
+++ lesson_6.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <ostream>
 
 int main() {
   // INFO: Arithmetic operators: return the result of specific Arithmetic
-  //       operation (+ - / *)
+  //       operation (+ - / * %)
+  //       + = plus
+  //       - = minus
+  //       / = divide
+  //       * = multiply
+  //       % = remainder
 
   // For example we have 20 blue caps
-  double blueCap = 20; // If you want to divide but you didn't get the decimal
-                       // portion then convert int to double
+  int blueCap = 20; // If you want to divide but you didn't get the decimal
+                    // portion then convert int to double
+                    // But also causes issues with % operator
 
   // 2 People bought caps
   blueCap = blueCap - 2;
@@ -17,8 +24,10 @@ int main() {
 
   // Let's check if we get decimals (Commented if you want to test it)
   /* blueCap /= 3; */
+  double remder = blueCap % 3;
 
-  std::cout << blueCap;
+  std::cout << blueCap << std::endl;
+  std::cout << remder;
 
   return 0;
 }
